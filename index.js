d65880bd6dd050465f8de39ca1d048430d70c150
rubens Rodrigues <rubens@schoolguardian.app>
2022-01-28 00:05:52 -0300

    Fix Heroku 6

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-require('dotenv').config();
+// require('dotenv').config();
 const express = require('express');
-const cors = require('cors')
-const mongoose = require('mongoose');
+// const cors = require('cors')
+// const mongoose = require('mongoose');
 
-const manageBooksRoutes = require('./routes/managePersons')
+// const manageBooksRoutes = require('./routes/managePersons')
 const testRoutes = require('./routes/test')
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true })) // x-www-form-urlencoded
-app.use(express.json())// json
-app.use(cors())
+// app.use(express.urlencoded({ extended: true })) // x-www-form-urlencoded
+// app.use(express.json())// json
+// app.use(cors())
 
-app.use('/persons', manageBooksRoutes);
+// app.use('/persons', manageBooksRoutes);
 app.use('/', testRoutes);
 
 console.log("Loading");
@@ -21,7 +21,10 @@ console.log(process.env.PORT);
 console.log(process.env.DATABASE_URL);
 var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function () {
+
+console.log(server_port);
+console.log(server_host);
+app.listen(server_port, server_host, function () {
     console.log('Listening on port %d', server_port);
 });
 
