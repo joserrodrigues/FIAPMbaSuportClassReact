34d736d45f892c8007a4354bec600558d073f207
rubens Rodrigues <rubens@schoolguardian.app>
2022-01-28 00:07:53 -0300

    Fix Heroku 7

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 // const mongoose = require('mongoose');
 
 // const manageBooksRoutes = require('./routes/managePersons')
-const testRoutes = require('./routes/test')
+const testRoutes = require('./routes/test.js')
 
 const app = express();
 
