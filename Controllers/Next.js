f46849f11bc571e31adc45b5e4e2148964b37fb6
rubens Rodrigues <rubens@schoolguardian.app>
2022-12-02 19:18:20 -0300

    Change To Upload

--- Controllers/Next.js
+++ Controllers/Next.js
@@ -4,7 +4,7 @@ exports.getItems = (req, res, next) => {
         { id: 1, name: "1" },
         { id: 2, name: "2" },
         { id: 3, name: "3" },
-        { id: 4, name: "4" },
+        { id: 4, name: "5" },
     ]);
 }
 
